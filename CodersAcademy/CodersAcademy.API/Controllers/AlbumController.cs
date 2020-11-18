834d900cb065588f79fa19b24f3daec9b358a192
Elizabeth <Carvalho>
2020-11-17 23:32:44 -0300

    [Aula 2] Primeira API criada .

---
+++ CodersAcademy/CodersAcademy.API/Controllers/AlbumController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodersAcademy.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AlbumController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetAlguns()
+		{
+			return Ok(new { Message = "Primeira API criada." });
+		}
+	}
+}
