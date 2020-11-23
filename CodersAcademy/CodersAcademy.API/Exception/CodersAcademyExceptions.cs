5a80e30d650f9008ada39444bfc8633c5df725cc
Elizabeth <Carvalho>
2020-11-22 21:04:11 -0300

    Clean code smell - pt2

--- CodersAcademy/CodersAcademy.API/Exception/CodersAcademyExceptions.cs
+++ CodersAcademy/CodersAcademy.API/Exception/CodersAcademyExceptions.cs
@@ -1,8 +1,17 @@
-﻿namespace CodersAcademy.API.Exception
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CodersAcademy.API.Exception
 {
+	[Serializable]
 	public class CodersAcademyExceptions : System.Exception
 	{
 		public CodersAcademyExceptions(string message) : base(message)
+		{
+
+		}
+
+		protected CodersAcademyExceptions(SerializationInfo info,StreamingContext streamingContext) : base(info, streamingContext)
 		{
 
 		}
